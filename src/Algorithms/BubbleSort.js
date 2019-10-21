1a08f15448911f32e1e653bbae4409633cc097a2
Chris Milson <chris.w.milson@gmail.com>
2019-10-21 17:06:11 +0900

    Send indicies to check before the ckeck

--- src/Algorithms/BubbleSort.js
+++ src/Algorithms/BubbleSort.js
@@ -1,13 +1,13 @@
 const BubbleSort = function * (values) {
   for (var i = 0; i < values.length; i++) {
     for (var j = 0; j < values.length - i - 1; j++) {
+      yield j
+      yield j + 1
       if (values[j] < values[j + 1]) {
         var temp = values[j]
         values[j] = values[j + 1]
         values[j + 1] = temp
       }
-      yield j
-      yield j + 1
       yield values
     }
   }
