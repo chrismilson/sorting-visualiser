939ee363d7d4fa741e78dd032e42102b61c0215c
Chris Milson <chris.w.milson@gmail.com>
2019-10-21 19:43:21 +0900

    Updates

--- precache-manifest.745e467a65f3954cb20199267b33b355.js
+++ precache-manifest.745e467a65f3954cb20199267b33b355.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9b0df7046e84fd8f65b5031cdcd27130",
+    "revision": "f82fa5cd4e6b9e5cee3d34449db587b5",
     "url": "/sorting-visualiser/index.html"
   },
   {
-    "revision": "0f90e65dd00fa8033096",
+    "revision": "f355f8cc6990a3a7987d",
     "url": "/sorting-visualiser/static/css/main.5b60a72e.chunk.css"
   },
   {
-    "revision": "24a4a08559e5c100cc53",
-    "url": "/sorting-visualiser/static/js/2.55ae4958.chunk.js"
+    "revision": "07ed199a368b5ad972a2",
+    "url": "/sorting-visualiser/static/js/2.4b850e3c.chunk.js"
   },
   {
-    "revision": "0f90e65dd00fa8033096",
-    "url": "/sorting-visualiser/static/js/main.b67eee3e.chunk.js"
+    "revision": "f355f8cc6990a3a7987d",
+    "url": "/sorting-visualiser/static/js/main.cd996916.chunk.js"
   },
   {
     "revision": "5b8d6a8eda33fbcf8fb3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sorting-visualiser/precache-manifest.69f7120be906c87da4b96c72e482bf26.js"
+  "/sorting-visualiser/precache-manifest.745e467a65f3954cb20199267b33b355.js"
 );
 
 self.addEventListener('message', (event) => {
