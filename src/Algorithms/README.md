0893846089a3fa2c7aff540ae2b713db962f0a27
Chris Milson <chris.w.milson@gmail.com>
2019-10-21 14:46:34 +0900

    Add bubble sort

---
+++ src/Algorithms/README.md
@@ -0,0 +1,10 @@
+# Algorithms
+
+These are the different sorting algorithms implemented in JavaScript.
+
+Each algorithm keeps an internal state and acts like an iterator. Each object
+implements a next function, which returns three objects:
+
+- The current array
+- A check index
+- A compare index
--- src/App.js
+++ src/App.js
@@ -2,26 +2,82 @@ import React from 'react'
 import './App.scss'
 
 import Array from './components/Array'
+import {
+  // MergeSort,
+  BubbleSort
+} from './Algorithms'
 
 class App extends React.Component {
   constructor (props) {
     super(props)
 
     var nums = []
-    for (var i = 0; i < 100; i++) {
+    for (var i = 0; i < 1000; i++) {
       nums.push(Math.random())
     }
 
     this.state = {
-      nums: nums
+      // mergeSort: new MergeSort(nums),
+      rate: 5,
+      nums: [...nums],
+      orig: nums
     }
+
+    this.sort = BubbleSort([...nums])
+
+    this.step = this.step.bind(this)
+    this.play = this.play.bind(this)
+    this.pause = this.pause.bind(this)
+    this.reset = this.reset.bind(this)
+  }
+
+  reset () {
+    this.setState({ check: -1, compare: -1, nums: [...this.state.orig] })
+    this.sort = BubbleSort([...this.state.orig])
+  }
+
+  step () {
+    var finished = false
+    this.setState(() => {
+      var check = this.sort.next().value
+      var compare = this.sort.next().value
+      var nums = this.sort.next().value
+      if (!nums) finished = true
+      return {
+        check,
+        compare,
+        nums: nums || this.state.nums
+      }
+    })
+    return finished
+  }
+
+  play () {
+    if (this.playing) return
+    this.player = setInterval(() => {
+      if (this.step()) {
+        clearInterval(this.player)
+        this.playing = false
+      }
+    }, this.state.rate)
+    this.playing = true
+  }
+
+  pause () {
+    clearInterval(this.player)
+    this.playing = false
   }
 
   render () {
     return (
       <div className='App'>
-        <div className='button' onClick={this.sort}>Sort</div>
-        <Array nums={this.state.nums} />
+        <div className='buttons'>
+          <div className='button' onClick={this.play}>Play</div>
+          <div className='button' onClick={this.pause}>Pause</div>
+          <div className='button' onClick={this.step}>Step</div>
+          <div className='button' onClick={this.reset}>Reset</div>
+        </div>
+        <Array {...this.state} />
       </div>
     )
   }
