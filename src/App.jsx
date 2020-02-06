7197fd23f1db9863c3b1f6e8619c4c913edf6c4e
Chris Milson <chris.w.milson@gmail.com>
2020-02-06 15:59:26 +0900

    Write basic boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App () {
+  return (
+    <div className='App'>
+      <h1>This is a React App.</h1>
+      <p>It is written in javascript and is built with webpack and babel.</p>
+    </div>
+  )
+}
