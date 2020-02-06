9852d17b90f0ac22149dfbe40201b161fbfe9043
Chris Milson <chris.w.milson@gmail.com>
2020-02-06 16:20:33 +0900

    Add styles
    We can use sass to style our page with some different loaders.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './App.scss'
 
 export default function App () {
   return (
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,18 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.s[ac]ss$/,
+        exclude: /node_modules/,
+        use: [
+          // And finally load the js into style nodes
+          'style-loader',
+          // Then compile the css to commonjs
+          'css-loader',
+          // First compile the sass to css
+          'sass-loader'
+        ] // NOTE: webpack runs the loaders from bottom to top
       }
     ]
   },
