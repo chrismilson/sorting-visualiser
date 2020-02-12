44840c4ad9d00445a251a78c55638fd3e69fe245
Chris Milson <chris.w.milson@gmail.com>
2020-02-12 13:47:51 +0900

    Update babel config
    Babel will not play nicely with async/await or generator functions. By
    setting the target for the env preset, babel will know how we want it to
    handle these special parts of our syntax.
    
    Since we will be doing work with workers, and workers are fundamentally
    async, we want to use the async/await in our app.

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,13 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+            presets: [
+              [
+                '@babel/preset-env',
+                { targets: { node: 10 } }
+              ],
+              '@babel/preset-react'
+            ]
           }
         }
       },
