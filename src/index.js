6bca82be5358c9025119b823ff38e3b14223fda8
Chris Milson <chris.w.milson@gmail.com>
2020-02-12 09:40:18 +0900

    Add test code
    We dont actually have to have a UI to interact with the state. This just
    calls some of the common store functions to get a feel for how they
    work.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,27 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { createStore } from 'redux'
 import todoApp from './state/reducers'
+import {
+  addTodo,
+  toggleTodo,
+  setVisibilityFilter,
+  VisibilityFilters
+} from './state/actions'
 import App from './App'
 
 const store = createStore(todoApp)
+
+console.log(store.getState())
+
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
+
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+unsubscribe()
 
 ReactDom.render(<App />, document.getElementById('root'))
