05956d2ed2ad2f225f611bebeb7f2bddb430068a
Chris Milson <chris.w.milson@gmail.com>
2020-02-12 10:32:06 +0900

    Implement container components
    The container components take the simple presentational components and
    connect them to the state.

---
+++ src/containers/FilterLink.js
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux'
+import Link from '../components/Link'
+import { setVisibilityFilter } from '../state/actions'
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    active: ownProps.filter === state.visibilityFilter
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onClick: () => {
+      dispatch(setVisibilityFilter(ownProps.filter))
+    }
+  }
+}
+
+const FilterLink = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Link)
+
+export default FilterLink
