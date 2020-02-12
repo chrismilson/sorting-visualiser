57cd263b25241f47936dcc131e4e73a025c2023d
Chris Milson <chris.w.milson@gmail.com>
2020-02-12 09:33:36 +0900

    Create store
    The store is the interface between our UI and our state.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { createStore } from 'redux'
+import todoApp from './state/reducers'
 import App from './App'
+
+const store = createStore(todoApp)
 
 ReactDom.render(<App />, document.getElementById('root'))
