8441992caa35ebce0fff262d548b4e2499ffa0b9
Chris Milson <chris.w.milson@gmail.com>
2020-02-12 13:53:27 +0900

    Wrap the remote store
    The remote store is inherrently async; not what redux is expecting, so
    we have to wrap it into something more natural.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { Provider } from 'react-redux'
 import { wrap } from 'comlink'
+import { remoteStoreWrapper } from 'redux-comlink'
 import App from './App'
 
-const remoteStore = wrap(new window.Worker('./state/store.worker.js'))
-const store = remoteStore
+const run = async () => {
+  const remoteStore = wrap(new Worker(
+    './state/store.worker.js',
+    { type: 'module' }
+  ))
+  const store = await remoteStoreWrapper(remoteStore)
 
-ReactDom.render(
-  <Provider store={store}><App /></Provider>,
-  document.getElementById('root')
-)
+  ReactDom.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+}
+run()
