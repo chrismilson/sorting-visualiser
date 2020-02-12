4c98483e61ba569f2f8f9735ba0850cb4b98361c
Chris Milson <chris.w.milson@gmail.com>
2020-02-12 10:46:22 +0900

    Provide store to app
    Finally we provide the store to the app through react-redux's Provider
    component.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todoApp from './state/reducers'
-import {
-  addTodo,
-  toggleTodo,
-  setVisibilityFilter,
-  VisibilityFilters
-} from './state/actions'
 import App from './App'
 
 const store = createStore(todoApp)
 
-console.log(store.getState())
-
-const unsubscribe = store.subscribe(() => console.log(store.getState()))
-
-store.dispatch(addTodo('Learn about actions'))
-store.dispatch(addTodo('Learn about reducers'))
-store.dispatch(addTodo('Learn about store'))
-store.dispatch(toggleTodo(0))
-store.dispatch(toggleTodo(1))
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
-
-unsubscribe()
-
-ReactDom.render(<App />, document.getElementById('root'))
+ReactDom.render(
+  <Provider store={store}><App /></Provider>,
+  document.getElementById('root')
+)
