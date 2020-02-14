1c946cb803f8036199221e8190f9c863cd4189a4
Chris Milson <chris.w.milson@gmail.com>
2020-02-14 14:58:39 +0900

    Use middleware on store
    Now that we have enabled the thunk middleware on our store, we dispatch
    the fetch action on load.

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,15 @@
 import reducer from './reducers'
-import { createStore } from 'redux'
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+import { selectSubreddit, fetchPosts } from './actions'
 
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk)
+)
+
+store.dispatch(selectSubreddit('reactjs'))
+store.dispatch(fetchPosts('reactjs'))
+  .then(() => console.log(store.getState()))
 
 export default store
