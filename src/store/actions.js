69da7ed40091b1e0c24d332ad6b92bfb98690389
Chris Milson <chris.w.milson@gmail.com>
2020-02-14 14:44:05 +0900

    Add async action creator
    Now that we have redux-thunk we can dispatch these new actions!
    
    This just executes a function that will call other synchronous actions
    in an asynchronous way.

--- src/store/actions.js
+++ src/store/actions.js
@@ -41,3 +41,13 @@ export function receivePosts (subreddit, json) {
     }
   }
 }
+
+// this is a thunk action creator; it returns a function, not an action object!
+export function fetchPosts (subreddit) {
+  return dispatch => {
+    dispatch(requestPosts(subreddit))
+    return fetch('https://www.reddit.com/r/${subreddit}.json')
+      .then(response => response.json())
+      .then(json => dispatch(receivePosts(subreddit, json)))
+  }
+}
