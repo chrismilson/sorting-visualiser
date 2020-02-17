5533d34917dbc5abcace415d926dfe086c345971
Chris Milson <chris.w.milson@gmail.com>
2020-02-17 13:28:03 +0900

    Add complexity analysis
    There is now a short explanation of the time and space complexity of the
    sort in the comments.

--- src/state/algorithms/bubble-sort.mjs
+++ src/state/algorithms/bubble-sort.mjs
@@ -1,5 +1,5 @@
 /**
- * Bubble sort is a sorting algorithm that will sort an array of comparable
+ * Bubble sort is a sorting algorithm that will sort an array of n comparable
  * elements.
  *
  * Bubble sort works by checking adjacent values. If the right value is less
@@ -7,7 +7,15 @@
  *
  * After one pass of the entire array from the end to the beginning, the
  * smallest element in the array will be at the front. Then the pairs are all
- * checked again (but not the definitely sorted part)
+ * checked again (but not the definitely sorted part).
+ *
+ * Since we only have one element sorted each pass, we will have to make a pass
+ * for each (next smallest) element in the array, leading to a 'sum of the
+ * numbers up to n' style time complexity, which is O(n^2).
+ *
+ * The space complexity however is very simple, we only keep track of the number
+ * of definitely sorted values and our position in the current pass. This means
+ * we have O(1) space complexity.
  * @param tracker
  */
 export default function bubbleSort (tracker) {
