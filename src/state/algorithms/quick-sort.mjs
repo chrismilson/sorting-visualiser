efa9200d0ac3f47579c377cd33466e967b31f564
Chris Milson <chris.w.milson@gmail.com>
2020-02-17 14:00:54 +0900

    Add more info in comments
    Added a more detailed explanation of how quick sort works and its
    complexity in the comments.

--- src/state/algorithms/quick-sort.mjs
+++ src/state/algorithms/quick-sort.mjs
@@ -2,6 +2,29 @@
  * Quick sort is a recursive sorting algorithm that will sort an array of n
  * comparable elements.
  *
+ * Quick sort checks the length of the array:
+ * - If the length is less than two, it doesn't have to do anything at all!
+ * - If the length is equal to two, it just swaps them if need be.
+ * - Otherwise it splits into two parts and runs quick sort on the smaller
+ *   subarrays:
+ *   - The values less than or equal to the very first value; and,
+ *   - The values greater than the very first value.
+ *
+ * Since the subarrays are smaller, the depth will be at most the length of the
+ * array (if the smallest value in the array is the first element in every
+ * subarray; the array was sorted already) and the sort will finish. Giving us a
+ * worst case of O(n^2) time complexity.
+ *
+ * On average for random data however the split will be closer to the middle and
+ * in the best case we will have half the values in each part. Thus in our best
+ * case we will have O(n log(n)) time complexity.
+ *
+ * Since we are recursing, we will have O(1) space for each layer in the
+ * recursion, so the worst case for our space complexity will be O(n).
+ *
+ * In the best case however we will have less layers alive at any one time,
+ * meaning our space complexity would be O(log(n)).
+ *
  * @param tracker
  */
 export default function quickSort (tracker) {
@@ -9,9 +32,9 @@ export default function quickSort (tracker) {
 
   const rec = (lo, hi) => {
     // base cases
-    if (hi - lo < 1) return
-    if (hi - lo === 1) {
-      if (lessThan(hi, lo)) swap(hi, lo)
+    if (hi - lo < 2) return
+    if (hi - lo === 2) {
+      if (lessThan(hi - 1, lo)) swap(hi - 1, lo)
       return
     }
 
