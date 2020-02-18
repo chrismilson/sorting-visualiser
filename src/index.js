19d27373b3f4a05771703556f30198b3c888f3e5
Chris Milson <chris.w.milson@gmail.com>
2020-02-18 14:19:14 +0900

    Provide the store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './state'
 import App from './App'
 
-ReactDom.render(<App />, document.getElementById('root'))
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
