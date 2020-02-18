39c63450fc1d000e1938bf4f7065f7858798b52d
Chris Milson <chris.w.milson@gmail.com>
2020-02-18 14:10:20 +0900

    Change tracking pattern
    Previously we have a Tracker class that was passed to the sorting
    algorithm and we *trusted* that the algorithm would not access the
    _values object on the tracker. Now we have a calculate method, that
    takes advantage of the js closure to only pass the intended apis to the
    sorting algorithm.
    
    This change has ALSO changed the pattern for calculating the moves for a
    sort. Previously we would create the tracker object and mutate it with
    the sorting algorithm. Now we pass both the algorithm and the values to
    the calculation method, and it will return the sort moves **without**
    mutating either the sorting algorithm *or* the values passed.

---
+++ src/state/algorithms/index.mjs
@@ -0,0 +1,70 @@
+import { MoveTypes } from '../actions'
+const { SWAP, COMPARE, START } = MoveTypes
+
+/**
+ * This method calculates a sequence of actions to sort the given data.
+ *
+ * @param sort The sorting algorithm
+ * @param origValues The original values array
+ */
+export function calculate (sort, origValues) {
+  const moves = [{
+    type: START,
+    payload: { values: origValues }
+  }]
+  const values = [...origValues]
+
+  /**
+   * Swaps the values at indicies i and j.
+   *
+   * @param {number} i
+   * @param {number} j
+   */
+  const swap = (i, j) => {
+    // swap the values
+    const temp = values[i]
+    values[i] = values[j]
+    values[j] = temp
+
+    // then record the move
+    moves.push({
+      type: SWAP,
+      payload: { i, j }
+    })
+  }
+
+  /**
+   * Returns a 0, -1 or 1 depending on the values at indicies i and j. Behaves
+   * in a similar manor to the compare function in Java.
+   *
+   * @param {number} i
+   * @param {number} j
+   */
+  const compare = (i, j) => {
+    const result = Math.sign(values[i] - values[j])
+
+    moves.push({
+      type: COMPARE,
+      payload: { i, j, result }
+    })
+
+    return result
+  }
+
+  const lessThan = (...args) => compare(...args) < 0
+  const lessThanEq = (...args) => compare(...args) <= 0
+  const greaterThan = (...args) => compare(...args) > 0
+  const greaterThanEq = (...args) => compare(...args) >= 0
+
+  // pass the different apis to the sort
+  sort({
+    swap,
+    compare,
+    lessThan,
+    lessThanEq,
+    greaterThan,
+    greaterThanEq
+  })
+
+  return moves
+}
