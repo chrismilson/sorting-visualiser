e64c11c88fd5682e711fb7aa555b8287ac1d9ca3
Chris Milson <chris.w.milson@gmail.com>
2020-03-25 15:26:09 +0900

    Extend expect
    This module will test sorting algorithms heavily; it makes sense to add
    some extensions to expect that can make the tests more readable.

---
+++ src/setupTests.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+import '@testing-library/jest-dom/extend-expect'
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toBeSorted: () => R
+    }
+  }
+}
+
+expect.extend({
+  toBeSorted: (received: number[]) => {
+    let pass = true
+    for (let i = 1; i < received.length; i++) {
+      if (received[i - 1] > received[i]) {
+        pass = false
+        break
+      }
+    }
+
+    const message = () => {
+      return `Expected ${received} to ${pass ? 'not ' : ''}be sorted`
+    }
+
+    return { message, pass }
+  }
+})
