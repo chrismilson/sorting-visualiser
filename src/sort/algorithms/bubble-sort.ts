7a6feb09e90e93ad3b3642e0a5202e541c871a83
Chris Milson <chris.w.milson@gmail.com>
2020-03-25 15:28:12 +0900

    Fix bug
    The scan was supposed to be from right to left instead of left to right.

--- src/sort/algorithms/bubble-sort.ts
+++ src/sort/algorithms/bubble-sort.ts
@@ -1,8 +1,8 @@
-import { Algorithm } from './Algorithm'
+import { Algorithm } from '.'
 
 const bubbleSort: Algorithm = ({ swap, compare, size }) => {
   for (let i = 0; i < size - 1; i++) {
-    for (let j = i + 1; j < size; j++) {
+    for (let j = size - 1; j > i; j--) {
       if (compare(j, j - 1) < 0) swap(j, j - 1)
     }
   }
