7afa951f82a3e0377bc2f1ed7c33af37a0ac1db8
Chris Milson <chris.w.milson@gmail.com>
2020-03-25 15:43:43 +0900

    Make implementation more readable

--- src/sort/algorithms/quick-sort.ts
+++ src/sort/algorithms/quick-sort.ts
@@ -1,36 +1,27 @@
 import { Algorithm } from '.'
 
 const quickSort: Algorithm = ({ swap, compare, size }) => {
-  const rec = (lo: number, hi: number) => {
-    // Base case is either:
-    // - There are no elements to check; or,
-    if (hi - lo < 2) return
-    // - There are two elements to check.
-    if (hi - lo === 2) {
-      if (compare(lo, lo + 1) > 0) swap(lo, lo + 1)
-      return
+  const partition = (low: number, high: number) => {
+    const pivot = low
+
+    let i = high
+    for (let j = high; j > pivot; j--) {
+      if (compare(pivot, j) < 0) swap(j, i--)
     }
 
-    const pivot = lo
-    let less = pivot + 1
-    let more = hi - 1
-    while (less < more) {
-      if (compare(pivot, less) < 0) {
-        swap(less, more)
-        more--
-      } else {
-        less++
-      }
-    }
-
-    // now we have two sections.
-    // lo <-> less are all less than or equal to pivot
-    // more <-> hi are all greater than pivot
-    rec(lo, less)
-    rec(more, hi)
+    swap(pivot, i)
+    return i
   }
 
-  rec(0, size)
+  const sort = (low: number, high: number) => {
+    if (low < high) {
+      const mid = partition(low, high)
+      sort(low, mid - 1)
+      sort(mid + 1, high)
+    }
+  }
+
+  sort(0, size - 1)
 }
 
 export default quickSort
