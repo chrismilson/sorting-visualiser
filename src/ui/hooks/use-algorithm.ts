78de9a1e2d66cbb474de3729628634f23965b77e
Chris Milson <chris.w.milson@gmail.com>
2020-03-26 15:36:39 +0900

    Add option to reset on creation
    The untracker will usually leave the values modified by the algorithm,
    but this flag will return the values to their original form after
    calculation.

--- src/ui/hooks/use-algorithm.ts
+++ src/ui/hooks/use-algorithm.ts
@@ -6,11 +6,19 @@ import Tracker from '../../sort/Tracker'
  * Performs an algorithm on the values via a tracker and returns the tracker
  * after the moves have been performed.
  */
-const useAlgorithm = (algorithm: Algorithm, values: number[]) => {
+const useAlgorithm = (
+  algorithm: Algorithm,
+  values: number[],
+  resetAfter = false
+) => {
   return useMemo(() => {
     const tracker = new Tracker(values)
     algorithm(tracker)
-    return tracker.untrack()
+
+    const untracker = tracker.untrack()
+    if (resetAfter) untracker.reset()
+
+    return untracker
   }, [algorithm, values])
 }
 
