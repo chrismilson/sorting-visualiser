8006a1b1fcb6e60a63cefc0c51542d71846d8db9
Chris Milson <chris.w.milson@gmail.com>
2020-03-26 09:01:42 +0900

    Add more general test cases
    Instead of adding random cases, just test all permutations of n.

--- src/sort/algorithms/index.test.ts
+++ src/sort/algorithms/index.test.ts
@@ -2,17 +2,41 @@ import Tracker from '../Tracker'
 import quickSort from './quick-sort'
 import bubbleSort from './bubble-sort'
 
+const permute = (n: number) => {
+  const numbers: number[] = []
+  for (let i = 0; i < n; i++) numbers.push(i)
+
+  const swap = (i: number, j: number) => {
+    const temp = numbers[i]
+    numbers[i] = numbers[j]
+    numbers[j] = temp
+  }
+
+  const permutations: number[][][] = []
+
+  const rec = (fixed: number) => {
+    if (n - fixed < 2) {
+      permutations.push([[...numbers]])
+      return
+    }
+    for (let i = fixed; i < n; i++) {
+      swap(fixed, i)
+      rec(fixed + 1)
+      swap(fixed, i)
+    }
+  }
+
+  rec(0)
+  return permutations
+}
+
 describe.each([
   ['quick', quickSort],
   ['bubble', bubbleSort]
 ])('%s sort', (_name, sort) => {
-  it.each([
-    [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
-    [1, 5, 6, 2, 2, 22, 3, 66, 7, 899, 3, 4, 1, 2333, 6, 5, 444, 3, 2, 55],
-    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-  ])('should sort data', (...values) => {
-    const tracker = new Tracker(values)
-    sort(tracker)
+  it.each(permute(3))('should sort %p', values => {
+    sort(new Tracker(values))
+
     expect(values).toBeSorted()
   })
 })
