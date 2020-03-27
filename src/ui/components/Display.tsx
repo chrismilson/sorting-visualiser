7f48655bf75f387c47a8bcccd7fafce518459845
Chris Milson <chris.w.milson@gmail.com>
2020-03-27 11:42:18 +0900

    Only repaint changed values
    Instead of repainting the entire canvas on every frame, we check the
    values that have changed and only repaint those values.

--- src/ui/components/Display.tsx
+++ src/ui/components/Display.tsx
@@ -12,26 +12,38 @@ const Display: React.FC<{
     ctx => {
       // resize the pixels, not just a maths transform
       ctx.canvas.width = ctx.canvas.height = values.length
-      ctx.setTransform(1, 0, 0, -values.length, 0.5, values.length)
+      ctx.setTransform(1, 0, 0, -values.length, 0, values.length)
 
       ctx.lineWidth = 1
 
-      ctx.strokeStyle = 'rgb(87,163,207)'
+      ctx.fillStyle = ctx.strokeStyle = 'rgb(87,163,207)'
+
       // ctx.strokeStyle = '#57A3CF'
+
+      const currentValues = [...values]
+      ctx.save()
+      ctx.translate(0.5, 0)
+      ctx.beginPath()
+      values.forEach((value, index) => {
+        ctx.moveTo(index, 0)
+        ctx.lineTo(index, value)
+      })
+      ctx.closePath()
+      ctx.stroke()
+      ctx.restore()
+
+      ctx.globalCompositeOperation = 'xor'
 
       let frame: number
       const drawFrame = () => {
-        ctx.clearRect(0, 0, values.length, 1)
-
-        ctx.beginPath()
-        values.forEach((value, index) => {
-          ctx.moveTo(index, 0)
-          ctx.lineTo(index, value)
-        })
-        ctx.closePath()
-        ctx.stroke()
-
         frame = requestAnimationFrame(drawFrame)
+        for (let i = 0; i < values.length; i++) {
+          if (currentValues[i] !== values[i]) {
+            ctx.clearRect(i, 0, 1, 1)
+            ctx.fillRect(i, 0, 1, values[i])
+            currentValues[i] = values[i]
+          }
+        }
       }
 
       drawFrame()
