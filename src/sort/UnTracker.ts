7be2527b7411ed5058aa01be169cf2d5ec0966f8
Chris Milson <chris.w.milson@gmail.com>
2020-03-30 08:42:45 +0900

    Avoid division by zero
    Perhaps there are no steps left to animate; in that case we will not
    animate.

--- src/sort/UnTracker.ts
+++ src/sort/UnTracker.ts
@@ -140,7 +140,7 @@ export default class Untracker {
         direction,
         onCompletion
       )
-    }
+    } else if (stepsRemaining === 0) return
 
     const timePerStep = timeUntilCompletion / stepsRemaining
     const interval = setInterval(() => {
