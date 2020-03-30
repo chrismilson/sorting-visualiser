d9a336ab9132271a2ee167949a0842ce9f23ff62
Chris Milson <chris.w.milson@gmail.com>
2020-03-30 11:04:06 +0900

    Add async useAlgorithm hook
    this is not truly async, but since it uses useEffect, it will allow the
    dom to render before calculating the algorithm.

--- src/ui/hooks/use-algorithm.ts
+++ src/ui/hooks/use-algorithm.ts
@@ -1,6 +1,7 @@
 import { Algorithm } from '../../sort/algorithms'
-import { useMemo } from 'react'
+import { useMemo, useState, useEffect } from 'react'
 import Tracker from '../../sort/Tracker'
+import Untracker from '../../sort/Untracker'
 
 /**
  * Performs an algorithm on the values via a tracker and returns the tracker
@@ -20,6 +21,27 @@ const useAlgorithm = (
 
     return untracker
   }, [algorithm, values, resetAfter])
+}
+
+export const useAlgorithmAsync = (algorithm: Algorithm, values: number[]) => {
+  const [untracker, setUntracker] = useState<Untracker>()
+
+  useEffect(() => {
+    // async effect returns promise, hence IIFE
+    ;(async () => {
+      // calculate on a copy
+      const tracker = new Tracker([...values])
+      algorithm(tracker)
+
+      // then replay on the real values
+      const untracker = tracker.untrack(values)
+      untracker.reset()
+
+      setUntracker(untracker)
+    })()
+  }, [algorithm, values])
+
+  return untracker
 }
 
 export default useAlgorithm
