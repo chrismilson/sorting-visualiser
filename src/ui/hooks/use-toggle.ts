b5f1a1fa8b3b70a37171b1576392a7a23ce85b07
Chris Milson <chris.w.milson@gmail.com>
2020-03-30 10:59:33 +0900

    Add set functionality
    The toggle can also be set to one of its values now.

--- src/ui/hooks/use-toggle.ts
+++ src/ui/hooks/use-toggle.ts
@@ -1,11 +1,16 @@
 import { useState, useCallback } from 'react'
 
 /** Toggles between two values */
-function useToggle<T>(first: T, second: T): [T, () => void] {
+function useToggle<T>(first: T, second: T): [T, (set?: T) => void] {
   const [value, setValue] = useState(true)
-  const toggle = useCallback(() => {
-    setValue(value => !value)
-  }, [])
+  const toggle = useCallback(
+    (firstOrSecond?: T) => {
+      if (firstOrSecond === first) setValue(true)
+      else if (firstOrSecond === second) setValue(false)
+      else setValue(value => !value)
+    },
+    [first, second]
+  )
 
   return [value ? first : second, toggle]
 }
