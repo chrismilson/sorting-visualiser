b803f441776617549df8be662b9aeee205669fa2
Chris Milson <chris.w.milson@gmail.com>
2020-09-09 11:23:45 +0900

    Use a single buffer
    Previously we were allocating two buffers. One for the set bits and one
    for the unset bits. One buffer will suffice for both, and it makes the
    algorithm easier to visualise as the separation into two bins grows from
    either side of the buffer.

--- src/sort/algorithms/sort/radix-sort-lsb.ts
+++ src/sort/algorithms/sort/radix-sort-lsb.ts
@@ -2,31 +2,29 @@ import { Algorithm } from '../../types'
 
 const lsbRadixSort: Algorithm = ({ nthBitSet, malloc, memcpy, free, size }) => {
   for (let needle = 0; 1 << needle <= size; needle++) {
-    const zeroBuffer = malloc(size)
+    const buffer = malloc(size)
     let zeroIndex = 0
-    const oneBuffer = malloc(size)
-    let oneIndex = 0
+    let oneIndex = size - 1
 
     // partition the values based on their binary representations.
     for (let i = 0; i < size; i++) {
       if (nthBitSet(i, needle)) {
-        memcpy(i, { buffer: oneBuffer, index: oneIndex++ })
+        memcpy(i, { buffer, index: oneIndex-- })
       } else {
-        memcpy(i, { buffer: zeroBuffer, index: zeroIndex++ })
+        memcpy(i, { buffer, index: zeroIndex++ })
       }
     }
 
     // put the values back in order.
     // first the zeros
     for (let i = 0; i < zeroIndex; i++) {
-      memcpy({ buffer: zeroBuffer, index: i }, i)
+      memcpy({ buffer, index: i }, i)
     }
-    free(zeroBuffer)
     // then the ones
-    for (let i = 0; i < oneIndex; i++) {
-      memcpy({ buffer: oneBuffer, index: i }, zeroIndex + i)
+    for (let i = 0; i < size - oneIndex; i++) {
+      memcpy({ buffer, index: size - i - 1 }, zeroIndex + i)
     }
-    free(oneBuffer)
+    free(buffer)
   }
 }
 
